db7ca427f0bf094f3e2811d26d96bf571f200d11
hcc <847404572@qq.com>
2018-11-30 15:34:34 +0800

    test

--- admin/public/webhooks.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$data = file_get_contents("php://input");
-file_put_contents("log",$data);
-exec("git pull");
---
+++ admin/public/webhooks1.php
@@ -0,0 +1,15 @@
+<?php
+	//秘钥
+	$token = "123456";
+
+	//接收github发送的通知内容
+	$json = file_get_contents("php://input");
+	$data = json_decode($json,true);
+
+	file_put_contents("log",$data);
+
+
+	//验证仓库密码是否正确
+	if($data['password']==$token){
+	    exec("git pull");
+	}
