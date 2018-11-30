fecbe7f95f88ec579b3cb7e53baa8eefd0a38b69
怀闯 <847404572@qq.com>
2018-11-30 20:04:48 +0800

    'edit_test.php'

--- test.php
+++ test.php
@@ -1,7 +1,6 @@
 <?php
 $key = "847404572a";
-$data = "sha1=0ab0de9de49c087e4b2d4dd173678837de423c91";
+$data = "sha1=8291c166e5861d6740fc688b9ee5dafa34463960";
 list($algo,$hash) = explode("=",$data);
 
 var_dump($hash == hash_hmac($algo,$hash,$key));
-
