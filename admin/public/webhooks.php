8d36629d2ff565b6ada86c055ee72715381a9761
怀闯 <847404572@qq.com>
2018-11-30 14:55:22 +0800

    'test'

--- admin/public/webhooks.php
+++ admin/public/webhooks.php
@@ -1,5 +1,16 @@
 <?php
 
-$data = file_get_contents("php://input");
-file_put_contents("log",$data);
-exec("git pull");
+//秘钥
+$token = "123456";
+
+//接收github发送的通知内容
+$json = file_get_contents("php://input");
+$data = json_decode($json,true);
+
+file_put_content("log",$data);
+
+
+//验证仓库密码是否正确
+if($data['password']==$token){
+    exec("git pull");
+}
