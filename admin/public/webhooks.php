846855571e5fdbdc4d5adc61e106d647f5991ca3
hcc <847404572@qq.com>
2018-11-30 16:58:52 +0800

    test

--- admin/public/webhooks.php
+++ admin/public/webhooks.php
@@ -1,13 +1,3 @@
 <?php
-	//秘钥
-	$token = "123456";
 
-	//接收github发送的通知内容
-	$json = file_get_contents("php://input");
-	$data = json_decode($json,true);
-
-	file_put_contents("log",$data);
-
-
-	//验证仓库密码是否正确
-	 exec("git pull");
+exec("git pull");
